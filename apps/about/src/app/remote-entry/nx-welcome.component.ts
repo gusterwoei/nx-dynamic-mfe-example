a5894b0cda44f7e345cb0af8d3427fe0f8992c50
Guster <gusterwoei@gmail.com>
2025-01-12 11:37:05 +0800

    add about page

--- apps/about/src/app/remote-entry/nx-welcome.component.ts
+++ apps/about/src/app/remote-entry/nx-welcome.component.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome shop 👋
+            Welcome about 👋
           </h1>
         </div>
         <!--  HERO  -->
@@ -769,11 +769,11 @@ import { CommonModule } from '@angular/common';
               Build, test and lint your app
             </summary>
             <pre><span># Build</span>
-nx build
+nx build 
 <span># Test</span>
-nx test
+nx test 
 <span># Lint</span>
-nx lint
+nx lint 
 <span># Run them together!</span>
 nx run-many -t build test lint</pre>
           </details>
@@ -794,7 +794,7 @@ nx run-many -t build test lint</pre>
               </svg>
               View project details
             </summary>
-            <pre>nx show project shop</pre>
+            <pre>nx show project about</pre>
           </details>
 
           <details>
--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'about',
+    loadChildren: () => import('about/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'cart',
     loadChildren: () => import('cart/Routes').then((m) => m!.remoteRoutes),
   },
