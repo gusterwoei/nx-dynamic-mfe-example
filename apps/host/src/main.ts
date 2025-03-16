6e28216097b94dcad04db4b685da3f8a1bec998f
Guster <gusterwoei@gmail.com>
2025-03-16 22:36:52 +0800

    code refactoring

--- apps/host/src/main.ts
+++ apps/host/src/main.ts
@@ -1,10 +1,10 @@
 // import('./bootstrap').catch((err) => console.error(err));
 import { setRemoteDefinitions } from '@nx/angular/mf';
 
-// NOTE: this is the key to make it work in AWS S3+CloudFront
-// make sure to prefix with /host
-// fetch('/module-federation.manifest.json') // for local development
-fetch('/host/module-federation.manifest.json')
+// NOTE: if you're hosting the module-federation.manifest.json inside a directory instead of the root folder in S3,
+// make sure to prefix with /host, this is the key to make it work in AWS S3+CloudFront
+// eg. fetch('/host/module-federation.manifest.json')
+fetch('/module-federation.manifest.json')
   .then((res) => res.json())
   .then((definitions) => setRemoteDefinitions(definitions))
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
--- build-deploy.sh
+++ build-deploy.sh
@@ -1,3 +1,4 @@
 nx build host --base-href=/host/
 nx run-many -t build --exclude=host
 aws s3 sync dist/apps/ s3://mfe-app-demo/ --delete
+aws s3 cp dist/apps/host/module-federation.manifest.json s3://mfe-app-demo/
